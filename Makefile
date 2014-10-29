fbc84822ebef89e0de49e0f7961505013d44c434
Miroslav Vasko <miroslav.vasko@cargo-partner.com>
2014-10-29 12:32:21 +0100

    Removing complexity of data docker images

--- Makefile
+++ Makefile
@@ -1,34 +1,18 @@
 all: clean build
 clean:
 	# pass
-build: run-httpd data-books books data-jenkins-home jenkins data-mac-server mac-server data-movies-images movies-images data-movies-db movies-db movies podcasts stop-httpd
-data-books:
-	cd containers
-	docker build data-books
-	cd ..
+build: run-httpd books jenkins mac-server movies-images movies-db movies podcasts stop-httpd
 books:
 	cd containers
 	docker build books
-	cd ..
-data-jenkins-home:
-	cd containers
-	docker build data-jenkins-home
 	cd ..
 jenkins:
 	cd containers
 	docker build jenkins
 	cd ..
-data-mac-server:
-	cd containers
-	docker build data-mac-server
-	cd ..
 mac-server:
 	cd containers
 	docker build mac-server
-	cd ..
-data-movies-images:
-	cd containers
-	docker build data-movies-images
 	cd ..
 movies-images:
 	cd containers
--- start.sh
+++ start.sh
@@ -6,21 +6,10 @@
 # podcasts/apache: 8083
 # mac-server: gets its own IP with ports exposed
 
-docker run -d --name=data-books data-books
-docker run -d -p 8081:80 --name=books --volumes-from=data-books books
-
-docker run -d --name=data-jenkins-home data-jenkins-home
-docker run -d -p 8082:8080 --name=jenkins --volumes-from=data-jenkins-home jenkins
-
-docker run -d --name=data-mac-server data-mac-server
-docker run -d --name=mac-server --volumes-from=data-mac-server mac-server
-
-docker run -d --name=data-movies-db data-movies-db
-docker run -d --name=movies-db --volumes-from=data-movies-db movies-db
-
-docker run -d --name=data-movies-image data-movies-images
-docker run -d --name=movies-image --volumes-from=data-movies-image --link=movies-db:movies-db movies-images
-
-docker run -d -p 8080:8080 --name=movies --link=movies-db:movies-db --link=movies-image:movies-image movies
-
+docker run -d -p 8081:80 --name=books books
+docker run -d -p 8082:8080 --name=jenkins jenkins
+docker run -d --name=mac-server mac-server
+docker run -d --name=movies-db movies-db
+docker run -d --name=movies-image --link=movies-db:movies-db movies-images
+docker run -d -p 8080:8080 --name=movies --link=movies-db:movies-db --link=movies-image:movies-image --volumes-from=movies-image movies
 docker run -d -d -p 8083:80 --name=podcasts podcasts
