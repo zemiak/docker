582e365c6cebed8086a66f1584f55b42d5f4065a
zemiak <zemiak@gmail.com>
2014-10-30 07:03:07 +0100

    Falling back to SimpleHTTPServer if busybox does not exist or does not contain httpd

--- httpd.sh
+++ httpd.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
-cd /mnt/media/docker-data/
-which busybox >/dev/null || exit 1
-busybox httpd -p 8000 -h /mnt/media/docker-data/
+which busybox >/dev/null
+
+if [ "$?" -eq "0" ]
+then
+	busybox httpd -p 8000 -h /mnt/media/docker-data/
+	if [ "$?" -eq "0" ]
+	then
+		exit 0
+	fi
+fi
+
+cd /mnt/media/docker-data
+python -m SimpleHTTPServer &
