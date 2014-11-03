fb4bac4e9bd218f01b7ece9bd46db392cb3eb94f
Miroslav Vasko <miroslav.vasko@cargo-partner.com>
2014-11-03 17:05:49 +0100

    MacServer setup

--- bin/install-mac-server.sh
+++ bin/install-mac-server.sh
@@ -1,17 +1,12 @@
-FROM debian:wheezy
-RUN echo "deb http://http.debian.net/debian/ wheezy-backports main contrib" >>/etc/apt/sources.list
-RUN apt-get update
-RUN apt-get install -yq linux-headers-$(uname -r|sed 's,[^-]*-[^-]*-,,')
-RUN apt-get -yq -t wheezy-backports install virtualbox
+#!/bin.sh
 
-ENV http_proxy ""
-ADD http://127.0.0.1:8000/mac-server/files.tar.gz /var/lib/virtualbox/
-WORKDIR /var/lib/virtualbox
-RUN tar -xzf files.tar.gz && rm -f files.tar.gz
+test -d /var/lib/virtualbox && exit 1
 
-# exporting, so it can be backed up
-VOLUMES /var/lib/virtualbox
-
-RUN VBoxManage registervm /var/lib/virtualbox/MacServer.vbox
-
-CMD VBoxHeadless --startvm MacServer
+echo "deb http://http.debian.net/debian/ wheezy-backports main contrib" >/etc/apt/sources.list.d/wheezy-backports
+apt-get update
+apt-get install -yq linux-headers-$(uname -r|sed 's,[^-]*-[^-]*-,,')
+apt-get -yq -t wheezy-backports install virtualbox
+mkdir -p /var/lib/vritualbox
+cd /var/lib/virtualbox
+tar -xzf /mnt/media/docker-data/mac-server/files.tar.gz
+VBoxManage registervm /var/lib/virtualbox/MacServer.vbox
