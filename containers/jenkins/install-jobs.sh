a394dd291c38e808028ad70317a581d9558f7513
vasko <vasko@lenovo-server.local>
2014-11-11 22:07:31 +0100

    Git still not working :(

--- containers/jenkins/install-jobs.sh
+++ containers/jenkins/install-jobs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 java $JAVA_OPTS -jar /usr/share/jenkins/jenkins.war $JENKINS_OPTS &
-sleep 10s
+sleep 30s
 
 curl -X POST -d '<jenkins><install plugin="git@2.0" /></jenkins>' --header 'Content-Type: text/xml' http://localhost:8080/pluginManager/installNecessaryPlugins
 curl -X POST -H "Content-Type:application/xml" -d @/tmp/movies-config.xml "http://localhost:8080/createItem?name=movies" 
