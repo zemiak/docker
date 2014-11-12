7edf0a8101c75844829deef04c9576470cc99fdd
Miroslav Vasko <vasko@docker-tryout.cp.local>
2014-11-12 05:20:52 -0500

    Jenkins fixes

--- containers/jenkins/install-jobs.sh
+++ containers/jenkins/install-jobs.sh
@@ -1,7 +1,23 @@
 #!/bin/sh
 
+# Run the Jenkins
 java $JAVA_OPTS -jar /usr/share/jenkins/jenkins.war $JENKINS_OPTS &
 sleep 30s
 
-curl -X POST -d '<jenkins><install plugin="git@2.0" /></jenkins>' --header 'Content-Type: text/xml' http://localhost:8080/pluginManager/installNecessaryPlugins
-curl -X POST -H "Content-Type:application/xml" -d @/tmp/movies-config.xml "http://localhost:8080/createItem?name=movies" 
+# Fetch the command line tools from running Jenkins
+curl --noproxy localhost http://localhost:8080/jnlpJars/jenkins-cli.jar >/usr/share/jenkins/jenkins-cli.jar
+
+# Install the GIT plugin
+http_proxy= java -jar /usr/share/jenkins/jenkins-cli.jar -s http://localhost:8080/ install-plugin /tmp/scm-api.hpi -deploy
+http_proxy= java -jar /usr/share/jenkins/jenkins-cli.jar -s http://localhost:8080/ install-plugin /tmp/git-client.hpi -deploy
+http_proxy= java -jar /usr/share/jenkins/jenkins-cli.jar -s http://localhost:8080/ install-plugin /tmp/git.hpi -deploy
+
+# Create the Movies job
+cat /tmp/movies-config.xml | java -jar /usr/share/jenkins/jenkins-cli.jar -s http://localhost:8080/ create-job movies
+
+# Configure the Jenkins (Maven, mailing)
+mv /tmp/*.xml /var/lib/jenkins
+java -jar /usr/share/jenkins/jenkins-cli.jar -s http://localhost:8080/ reload-configuration
+
+
+killall java
