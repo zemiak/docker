a881e4897f9c891ee8a4e392f3c07fd2fd4a1f49
zemiak <zemiak@gmail.com>
2014-11-18 21:07:26 +0100

    Mount points are better with labels

--- local/mount-shares/install.sh
+++ local/mount-shares/install.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
 cat /etc/fstab | grep media && exit 1
-cat "/dev/sdb2 /mnt/media ext4 defaults,noatime,noexec 0 0" >>/etc/fstab
-cat "/dev/sdb3 /mnt/timemachine-retina ext4 defaults,noatime,noexec 0 0" >>/etc/fstab
+cat "LABEL=media /mnt/media ext4 defaults,noatime,noexec 0 0" >>/etc/fstab
+cat "LABEL=timemachine-retina /mnt/timemachine-retina ext4 defaults,noatime,noexec 0 0" >>/etc/fstab
+cat "LABEL=docker-data /mnt/docker-data ext4 defaults,noatime,noexec 0 0" >>/etc/fstab
+cat "LABEL=backup-media /mnt/backup-media ext4 defaults,noatime,noexec 0 0" >>/etc/fstab
+cat "LABEL=backup-server /mnt/backup-server ext4 defaults,noatime,noexec 0 0" >>/etc/fstab
+
+mkdir /mnt/media
+mkdir /mnt/timemachine-retina
+mkdir /mnt/docker-data
+mkdir /mnt/backup-media
+mkdir /mnt/backup-server
+
 mount /mnt/media
 mount /mnt/timemachine-retina
