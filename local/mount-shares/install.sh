4474abec6ec65d8c5b88cdc7c7c8fe977a82a89f
vasko <vasko@lenovo-server.local>
2014-12-18 19:31:49 +0100

    TimeMachine label was too long

--- local/mount-shares/install.sh
+++ local/mount-shares/install.sh
@@ -2,7 +2,7 @@
 
 cat /etc/fstab | grep media && exit 1
 cat "LABEL=media /mnt/media ext4 defaults,noatime,noexec 0 0" >>/etc/fstab
-cat "LABEL=timemachine-retina /mnt/timemachine-retina ext4 defaults,noatime,noexec 0 0" >>/etc/fstab
+cat "LABEL=timemachine-reti /mnt/timemachine-retina ext4 defaults,noatime,noexec 0 0" >>/etc/fstab
 cat "LABEL=docker-data /mnt/docker-data ext4 defaults,noatime,noexec 0 0" >>/etc/fstab
 cat "LABEL=backup-media /mnt/backup-media ext4 defaults,noatime,noexec 0 0" >>/etc/fstab
 cat "LABEL=backup-server /mnt/backup-server ext4 defaults,noatime,noexec 0 0" >>/etc/fstab
