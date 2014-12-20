27adeb2c50a00e81a0b89c3f154215489765099f
zemiak <zemiak@gmail.com>
2014-12-20 10:02:10 +0100

    Simplified run scripts for movies

--- containers/movies/run.sh
+++ containers/movies/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Database
-/usr/lib/postgresql/9.3/bin/postgres -D /var/lib/postgresql/9.3/main -c config_file=/etc/postgresql/9.3/main/postgresql.conf &
+su postgres -c "/usr/lib/postgresql/9.3/bin/postgres -D /var/lib/postgresql/9.3/main -c config_file=/etc/postgresql/9.3/main/postgresql.conf" &
 
 # Movie Player
 service apache2 start
@@ -9,4 +9,3 @@ sleep 15s
 
 # Wildfly Application Server with the movies application
 /opt/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 &
-sleep 15s
