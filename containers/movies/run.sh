9ab24622aeddd6f4c2ad2be3475106a9b281e53a
vasko <vasko@lenovo-server.local>
2014-12-21 17:57:30 +0100

    Movies container builds

--- containers/movies/run.sh
+++ containers/movies/run.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
 # Database
-su postgres -c "/usr/lib/postgresql/9.3/bin/postgres -D /var/lib/postgresql/9.3/main -c config_file=/etc/postgresql/9.3/main/postgresql.conf" &
+service postgresql start
 
 # Movie Player
 service apache2 start
-sleep 15s
 
 # Wildfly Application Server with the movies application
 /opt/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 &
