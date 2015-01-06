3c2b638fad62c389aa96268ecfa629ef2ea3c073
vasko <vasko@lenovo-server.local>
2015-01-06 21:28:26 +0100

    Added mail sending to Movies

--- containers/movies/run-in-docker.sh
+++ containers/movies/run-in-docker.sh
@@ -9,8 +9,8 @@ service postgresql start
 # Movie Player
 service apache2 start
 
-# Wildfly Application Server with the movies application
-/opt/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 &
-sleep 10s
+# Mail sending
+service postfix start
 
-/bin/bash
+# Wildfly Application Server with the movies application
+/opt/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0
