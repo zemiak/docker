737986e1e9eed3e68fe6f4dcc89e34fac45ea631
Miroslav Vasko <miroslav.vasko@cargo-partner.com>
2015-01-20 11:05:01 +0100

    non-interactive postfix installation

--- local/mail/install.sh
+++ local/mail/install.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 
 echo ... installing mail system
+export DEBIAN_FRONTEND=noninteractive
 apt-get -yq install postfix >/dev/null
 cp main.cf /etc/postfix/
 cp /mnt/media/docker-data/mail/sasl_password /etc/postfix/
