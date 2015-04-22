ccb83938e508f7540b3801d5b8096fd44228c65b
Miroslav Vasko <miroslav.vasko@cargo-partner.com>
2015-04-22 17:16:49 +0200

    Sonarqube /2

--- containers/sonarqube/install-jobs.sh
+++ containers/sonarqube/install-jobs.sh
@@ -4,8 +4,8 @@ service mysql start
 /opt/sonarqube/bin/linux-x86-64/sonar.sh console &
 sleep 20s
 
-curl -u admin:admin -data='{}' -X POST "http://localhost:9000/api/projects/create?key=org.zemiak.movies:movies&name=movies"
-curl -u admin:admin -data='{}' -X POST "http://localhost:9000/api/projects/create?key=org.zemiak.online:online&name=online"
+http_proxy= curl -u admin:admin -data='{}' -X POST "http://127.0.0.1:9000/api/projects/create?key=org.zemiak.movies:movies&name=movies"
+http_proxy= curl -u admin:admin -data='{}' -X POST "http://127.0.0.1:9000/api/projects/create?key=org.zemiak.online:online&name=online"
 
 killall java
 service mysql stop
