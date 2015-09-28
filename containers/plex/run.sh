a64869cad5a271193472207fb5f5b940df17a6e4
zemiak <zemiak@gmail.com>
2015-09-28 21:13:18 +0200

    Plex run.sh altered to accomodate new folder structure

--- containers/plex/run.sh
+++ containers/plex/run.sh
@@ -6,4 +6,6 @@ then
     VERSION = 1_0_0
 fi
 
-docker run -d -p 80:80 -p 443:443 -p 2206:22 -p 32400:32400 -p 53:53/udp -p 53:53 --name=plex -v /mnt/media/:/data plex:$VERSION
+docker run -d -p 80:80 -p 443:443 -p 2206:22 -p 32400:32400 -p 53:53/udp -p 53:53 \
+    -v /mnt/media/:/mnt/media -v /mnt/plex/:/data \
+    --name=plex plex:$VERSION
