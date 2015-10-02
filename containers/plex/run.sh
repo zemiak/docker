736fc3770037c5fc94d044bba1d5d554dad2bcbd
zemiak <zemiak@gmail.com>
2015-10-02 23:00:33 +0200

    Library creation automation

--- containers/plex/run.sh
+++ containers/plex/run.sh
@@ -6,8 +6,9 @@ then
     VERSION=1_0_0
 fi
 
-mkdir -p /mnt/plex/data "/mnt/plex/config/Library/Application Support/Plex Media Server/"
-cp Preferences.xml "/mnt/plex/config/Library/Application Support/Plex Media Server/"
+mkdir -p /mnt/plex/data /mnt/plex/data/Movies /mnt/plex/data/Music
+mkdir -p /mnt/plex/data/Pictures /mnt/plex/data/TVShows /mnt/plex/data/HomeVideos
+mkdir -p "/mnt/plex/config/Library/Application Support/Plex Media Server/"
 docker run -d -p 80:80 -p 443:443 -p 2206:22 -p 32400:32400 -p 53:53/udp -p 53:53 \
     -v /mnt/media/:/mnt/media -v /mnt/plex/data/:/data -v /mnt/plex/config/:/config \
     --name=plex plex:$VERSION
--- containers/plex/start.sh
+++ containers/plex/start.sh
@@ -7,4 +7,19 @@ service ssh start
 cd /opt/plexconnect
 python ./PlexConnect.py >>/var/log/appletv-plex-connect.log &
 
-/usr/sbin/start_pms >>/var/log/plex-media-server.log
+if [ ! -f /data/CONFIGURED ]
+then
+    mv /tmp/Preferences.xml "/mnt/plex/config/Library/Application Support/Plex Media Server/"
+fi
+
+/usr/sbin/start_pms >>/var/log/plex-media-server.log &
+sleep 4s
+if [ ! -f /data/CONFIGURED ]
+then
+    curl -X POST 'http://127.0.0.1:32400/library/sections?type=movie&agent=com.plexapp.agents.imdb&scanner=Plex+Movie+Scanner&language=en&name=Movies&location=%2Fdata%2FMovies' >/data/MOVIES_CREATED
+    curl -X POST 'http://127.0.0.1:32400/library/sections?type=artist&agent=com.plexapp.agents.lastfm&scanner=Plex+Music+Scanner&language=en&name=Music&location=%2Fdata%2FMusic' >/data/MUSIC_CREATED
+    curl -X POST 'http://127.0.0.1:32400/library/sections?type=photo&agent=com.plexapp.agents.none&scanner=Plex+Photo+Scanner&language=xn&name=Photos&location=%2Fdata%2FPictures' >/data/PHOTOS_CREATED
+    curl -X POST 'http://127.0.0.1:32400/library/sections?type=show&agent=com.plexapp.agents.thetvdb&scanner=Plex+Series+Scanner&language=en&name=TV+Shows&location=%2Fdata%2FTVShows' >/data/TVSHOWSH_CREATED
+    curl -X POST 'http://127.0.0.1:32400/library/sections?type=movie&agent=com.plexapp.agents.none&scanner=Plex+Video+Files+Scanner&language=xn&name=Home+Videos&location=%2Fdata%2FHomeVideos' >/data/HOMEVIDEOS_CREATED
+    touch /data/CONFIGURED
+fi
