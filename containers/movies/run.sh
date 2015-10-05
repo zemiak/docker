b2c8446d1e23465740aadf6864b82f450298fa7f
zemiak <zemiak@gmail.com>
2015-10-05 22:13:48 +0200

    Music links, defined

--- containers/movies/run.sh
+++ containers/movies/run.sh
@@ -9,6 +9,7 @@ fi
 mkdir -p /mnt/plex
 docker run -d -p 8080:8080 -p 2200:22 --name=movies \
     -v /mnt/media/Movies:/mnt/media/Movies \
+    -v /mnt/media/Music:/mnt/media/Music \
     -v /mnt/plex/data:/mnt/plex/data \
     -v /mnt/media/Pictures/Movies:/mnt/media/Pictures/Movies \
     -v /mnt/media/backup/pgsql:/mnt/backup \
