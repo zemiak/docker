e37c0ff552cb354bb0314e9e5227ac3778fe9cec
Miroslav Vasko <miroslav.vasko@cargo-partner.com>
2015-12-15 10:00:17 +0100

    Including daapd server

--- containers/movies/start.sh
+++ containers/movies/start.sh
@@ -4,9 +4,16 @@ export JAVA_HOME=/opt/jdk
 export PATH=$PATH:/opt/jdk/bin
 
 dpkg-reconfigure ntp
-service postfix start
-service ssh start
-service ntp start
+/etc/init.d/postfix start
+/etc/init.d/ssh start
+/etc/init.d/ntp start
+
+# Forked-DAAPD (iTunes Music Server)
+/etc/init.d/dbus start
+/etc/init.d/avahi-daemon start
+/usr/sbin/forked-daapd -f -c /etc/forked-daapd.conf
+/etc/init.d/avai-daemon stop
+/etc/init.d/dbus stop
 
 # Wildfly Application Server with the movies application
 /opt/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0
