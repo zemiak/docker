57955c82d072178624c5ceefe9243b147c5e3983
Miroslav Vasko <miroslav.vasko@cargo-partner.com>
2015-12-15 09:13:15 +0100

    Removed Plex

--- containers/movies/run.sh
+++ containers/movies/run.sh
@@ -3,19 +3,16 @@
 VERSION=$1
 if [ -z "$VERSION" ]
 then
-    VERSION=3_0_0
+    VERSION=3_1_0
 fi
 
-mkdir -p "/mnt/plex/data/TV Shows" /mnt/plex/data/Movies /mnt/plex/data/Music \
-    /mnt/plex/data/Photos
-mkdir -p "/mnt/plex/config/Library/Application Support/Plex Media Server/"
-docker run -d -p 8080:8080 -p 2200:22 -p 32400:32400 -p 32469:32469 -p 1900:1900/udp \
-    -p 32410:32410/udp -p 32412:32412/udp -p 32413:32413/udp -p 32414:32414/udp \
+mkdir -p /mnt/media/0-Filmy
+docker run -d -p 8080:8080 -p 2200:22 \
     --name=movies \
     -v /mnt/media/Movies:/mnt/media/Movies \
     -v /mnt/media/Music:/mnt/media/Music \
     -v /mnt/media/backup/movies-database:/mnt/backup \
     -v /mnt/media/Pictures:/mnt/media/Pictures \
-    -v /mnt/plex/data/:/data \
+    -v /mnt/media/0-Filmy:/mnt/media/infuse \
     -v /mnt/plex/config/:/config
     movies:$VERSION
