9b8d03f3c33a0223fec2e32f64f6c4e6751f3d60
zemiak <zemiak@gmail.com>
2016-05-20 22:04:47 +0200

    Debug enabled, default admin password set for wildfly

--- containers/templates/wildfly/start-wildfly.sh
+++ containers/templates/wildfly/start-wildfly.sh
@@ -4,4 +4,5 @@ export JAVA_HOME=/opt/jdk
 export PATH=$PATH:/opt/jdk/bin
 
 /usr/local/bin/start-debian.sh
-/opt/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0
+/opt/wildfly/bin/add-user.sh -m -u admin -p admin
+/opt/wildfly/bin/standalone.sh --debug -b 0.0.0.0 -bmanagement 0.0.0.0
