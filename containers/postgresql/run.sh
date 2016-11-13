39284b68fb092e38afdbe9103be9b0b1975974ea
zemiak <zemiak@gmail.com>
2016-11-13 13:05:54 +0100

    Postgresq: SSH exposed

--- containers/postgresql/run.sh
+++ containers/postgresql/run.sh
@@ -6,7 +6,7 @@ then
     VERSION=1_0_0
 fi
 
-docker run -d -p 5432:5432 \
+docker run -d -p 5432:5432 -p 5422:22 \
     --name=postgresql \
     -v /mnt/media/postgresql/backups/:/mnt/backups \
     postgresql:$VERSION
--- containers/postgresql/start.sh
+++ containers/postgresql/start.sh
@@ -6,9 +6,9 @@ sh /usr/local/bin/start-debian.sh
 
 if [ ! -f $DATA_DIR/CONFIGURED ]
 then
-    psql -h 127.0.0.1 -U postgres -w --command "CREATE DATABASE movies WITH ENCODING 'UTF8' TEMPLATE template0 OWNER postgres;"
-    zcat /tmp/movies.plain.gz | psql -h 127.0.0.1 -U postgres -w movies
-    rm /tmp/movies.plain.gz
+    psql -h 127.0.0.1 -U postgres -w --command "CREATE DATABASE movies WITH ENCODING 'UTF8' TEMPLATE template0 OWNER postgres;" || exit 1
+    zcat /tmp/movies.plain.gz | psql -h 127.0.0.1 -U postgres -w movies || exit 2
+    rm /tmp/movies.plain.gz || exit 3
     
     echo "Movies DB restored"
     touch $DATA_DIR/CONFIGURED
