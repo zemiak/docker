7adca7255b707f35bcd47481b4cc6621f0cf428f
zemiak <zemiak@gmail.com>
2016-11-15 18:41:12 +0100

    Back to payara: wildfly not needed anymore

--- containers/templates/install.sh
+++ containers/templates/install.sh
@@ -10,9 +10,7 @@ cd debian
 docker build --tag=zemiak/debian . || exit 1
 cd ../java
 docker build --tag=zemiak/java . || exit 2
-cd ../wildfly
-docker build --tag=zemiak/wildfly . || exit 3
 cd ../payara
-docker build --tag=zemiak/payara . || exit 4
+docker build --tag=zemiak/payara . || exit 3
 
 cd "$FOLDER"
--- containers/templates/wildfly/start-wildfly.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-export JAVA_HOME=/opt/jdk
-export PATH=$PATH:/opt/jdk/bin
-
-/usr/local/bin/start-debian.sh
-/opt/wildfly/bin/add-user.sh -m -u admin -p admin
-/opt/wildfly/bin/standalone.sh --debug -b 0.0.0.0 -bmanagement 0.0.0.0
