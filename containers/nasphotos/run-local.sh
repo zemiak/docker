289b2953db406f2f1d23a29aacec0d15b5c07208
zemiak <zemiak@gmail.com>
2016-11-15 18:36:35 +0100

    nasphotos: back to payara

--- containers/nasphotos/run-local.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-docker run -d -p 8081:8080 -p 2206:22 \
-    --name=nasphotos \
-    -v /Volumes/media/Pictures:/pictures \
-    -v /Volumes/media/Pictures/cache:/cache \
-    nasphotos
