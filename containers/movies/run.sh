a9094648adb666a597381238a9e55e634e65565d
zemiak <zemiak@gmail.com>
2016-12-05 21:05:15 +0100

    Stateless container for movies

--- containers/movies/run.sh
+++ containers/movies/run.sh
@@ -3,11 +3,19 @@
 VERSION=$1
 if [ -z "$VERSION" ]
 then
-    VERSION=4_0_0
+    VERSION=4_1_0
 fi
 
 mkdir -p /mnt/media/infuse
-docker run --add-host mail:192.168.2.10 --add-host database:192.168.2.10 -d -p 8080:8080 -p 2200:22 \
+docker run -d \
+    -e "BIN_PATH=/usr/bin" \
+    -e "MEDIA_PATH=/mnt/media" \
+    -e "SYSTEM_NAME=prod" \
+    -e "MAIL_TO=zemiak@gmail.com" \
+    -e "EXTERNAL_URL=http://lenovo-server.local:8080/movies/" \
+    --add-host mail:192.168.2.10 \
+    --add-host database:192.168.2.10 \
+    -p 8080:8080 -p 2200:22 \
     --name=movies \
     -v /mnt/media:/mnt/media \
     movies:$VERSION
--- containers/movies/start.sh
+++ containers/movies/start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+/etc/init.d/cron start
 sh /usr/local/bin/start-payara.sh
 
 tail -f /etc/hosts
