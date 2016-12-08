5751f47708a9945ee85c7c85fd89c1e70dc65a4d
zemiak <zemiak@gmail.com>
2016-12-08 19:42:32 +0100

    Stateless nasphotos

--- containers/nasphotos/run.sh
+++ containers/nasphotos/run.sh
@@ -3,11 +3,16 @@
 VERSION=$1
 if [ -z "$VERSION" ]
 then
-    VERSION=1_0_0
+    VERSION=1_1_0
 fi
 
 docker run -d -p 8081:8080 -p 2206:22 \
     --name=nasphotos \
+    -e "BIN_PATH=/usr/bin" \
+    -e "PHOTO_PATH=/pictures" \
+    -e "TEMP_PATH=/cache" \
+    -e "WATERMARK_PATH=/opt/watermarks" \
+    -e "EXTERNAL_URL=http://lenovo-server.local:8081/nasphotos/" \
     -v /mnt/media/Pictures:/pictures \
     -v /mnt/media/Pictures/cache:/cache \
     nasphotos:$VERSION
--- images.js
+++ images.js
@@ -2,7 +2,7 @@
 
 var Images = {
     IMAGES: {
-	"nasphotos": "1_0_0",
+	"nasphotos": "1_1_0",
 	"movies": "4_1_0",
 	"books": "1_0_0",
 	"podcasts": "2_1_0",
